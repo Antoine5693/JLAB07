99fa415cf8ba5e1556671f7eeef6f017b7fe1f88
Louis Ducher <louis.ducher@epfedu.fr>
2026-03-16 12:28:15 +0100

    ajout bullet

--- src/js/selection.js
+++ src/js/selection.js
@@ -3,101 +3,117 @@
 var player; // désigne le sprite du joueur
 var groupe_plateformes; // contient toutes les plateformes
 var clavier; // pour la gestion du clavier
-
+var bullets; // groupe de projectiles tirés par le joueur
 export default class selection extends Phaser.Scene {
   constructor() {
-     super({key : "selection"});}
-
-preload() {
-  // tous les assets du jeu sont placés dans le sous-répertoire src/assets/
-  this.load.image("img_ciel", "src/assets/sky.png");
-  this.load.image("img_plateforme", "src/assets/platform.png");
-  this.load.spritesheet("img_perso", "src/assets/dude.png", {
-    frameWidth: 32,
-    frameHeight: 48
-  });
-}
-
-create() {
- 
-  this.add.image(400, 300, "img_ciel");
-
-  groupe_plateformes = this.physics.add.staticGroup();
- 
-  groupe_plateformes.create(200, 584, "img_plateforme");
-  groupe_plateformes.create(600, 584, "img_plateforme");
-
-  //  on ajoute 3 platesformes flottantes
-  groupe_plateformes.create(600, 450, "img_plateforme");
-  groupe_plateformes.create(50, 300, "img_plateforme");
-  groupe_plateformes.create(750, 270, "img_plateforme");
-
-  /****************************
-   *  CREATION DU PERSONNAGE  *
-   ****************************/
-
-  player = this.physics.add.sprite(100, 450, "img_perso");
-  player.setBounce(0.2); // on donne un petit coefficient de rebond
-  player.setCollideWorldBounds(true); // le player se cognera contre les bords du monde
-
-  this.anims.create({
-    key: "anim_tourne_gauche", // key est le nom de l'animation : doit etre unique poru la scene.
-    frames: this.anims.generateFrameNumbers("img_perso", { start: 0, end: 3 }), // on prend toutes les frames de img perso numerotées de 0 à 3
-    frameRate: 10, // vitesse de défilement des frames
-    repeat: -1 // nombre de répétitions de l'animation. -1 = infini
-  });
-
-  // creation de l'animation "anim_tourne_face" qui sera jouée sur le player lorsque ce dernier n'avance pas.
-  this.anims.create({
-    key: "anim_face",
-    frames: [{ key: "img_perso", frame: 4 }],
-    frameRate: 20
-  });
-
-  // creation de l'animation "anim_tourne_droite" qui sera jouée sur le player lorsque ce dernier tourne à droite
-  this.anims.create({
-    key: "anim_tourne_droite",
-    frames: this.anims.generateFrameNumbers("img_perso", { start: 5, end: 8 }),
-    frameRate: 10,
-    repeat: -1
-  });
-
- 
-  clavier = this.input.keyboard.createCursorKeys();
-
- 
-
-  //  Collide the player and the groupe_etoiles with the groupe_plateformes
-  this.physics.add.collider(player, groupe_plateformes);
-}
-
-update() {
-  player.setVelocityX(0);
-  player.setVelocityY(0);
-
-  // horizontal
-  if (clavier.left.isDown) {
-    player.setVelocityX(-160);
-    player.anims.play("anim_tourne_gauche", true);
-  } else if (clavier.right.isDown) {
-    player.setVelocityX(160);
-    player.anims.play("anim_tourne_droite", true);
+    super({ key: "selection" });
   }
 
-  // vertical
-  if (clavier.up.isDown) {
-    player.setVelocityY(-160);
-  } else if (clavier.down.isDown) {
-    player.setVelocityY(160);
+  preload() {
+    // tous les assets du jeu sont placés dans le sous-répertoire src/assets/
+    this.load.image("img_ciel", "src/assets/sky.png");
+    this.load.image("img_plateforme", "src/assets/platform.png");
+    this.load.spritesheet("img_perso", "src/assets/dude.png", {
+      frameWidth: 32,
+      frameHeight: 48
+    });
+    this.load.image("img_rondblanc", "src/assets/rondblanc.png");
   }
 
-  // idling
-  if (player.body.velocity.x === 0 && player.body.velocity.y === 0) {
-    player.anims.play("anim_face", true);
+  create() {
+
+    this.add.image(400, 300, "img_ciel");
+
+    groupe_plateformes = this.physics.add.staticGroup();
+
+    groupe_plateformes.create(200, 584, "img_plateforme");
+    groupe_plateformes.create(600, 584, "img_plateforme");
+
+    //  on ajoute 3 platesformes flottantes
+    groupe_plateformes.create(600, 450, "img_plateforme");
+    groupe_plateformes.create(50, 300, "img_plateforme");
+    groupe_plateformes.create(750, 270, "img_plateforme");
+    bullets = this.physics.add.group();
+
+    /****************************
+     *  CREATION DU PERSONNAGE  *
+     ****************************/
+
+    player = this.physics.add.sprite(100, 450, "img_perso");
+    player.setBounce(0.2); // on donne un petit coefficient de rebond
+    player.setCollideWorldBounds(true); // le player se cognera contre les bords du monde
+
+    this.anims.create({
+      key: "anim_tourne_gauche", // key est le nom de l'animation : doit etre unique poru la scene.
+      frames: this.anims.generateFrameNumbers("img_perso", { start: 0, end: 3 }), // on prend toutes les frames de img perso numerotées de 0 à 3
+      frameRate: 10, // vitesse de défilement des frames
+      repeat: -1 // nombre de répétitions de l'animation. -1 = infini
+    });
+
+    // creation de l'animation "anim_tourne_face" qui sera jouée sur le player lorsque ce dernier n'avance pas.
+    this.anims.create({
+      key: "anim_face",
+      frames: [{ key: "img_perso", frame: 4 }],
+      frameRate: 20
+    });
+
+    // creation de l'animation "anim_tourne_droite" qui sera jouée sur le player lorsque ce dernier tourne à droite
+    this.anims.create({
+      key: "anim_tourne_droite",
+      frames: this.anims.generateFrameNumbers("img_perso", { start: 5, end: 8 }),
+      frameRate: 10,
+      repeat: -1
+    });
+
+
+    clavier = this.input.keyboard.createCursorKeys();
+
+
+
+    //  Collide the player and the groupe_etoiles with the groupe_plateformes
+    this.physics.add.collider(player, groupe_plateformes);
   }
 
-  if (Phaser.Input.Keyboard.JustDown(clavier.space)) {
-    this.scene.start("niveau1");
+  update() {
+    player.setVelocityX(0);
+    player.setVelocityY(0);
+
+    // horizontal
+    if (clavier.left.isDown) {
+      player.setVelocityX(-160);
+      player.anims.play("anim_tourne_gauche", true);
+    } else if (clavier.right.isDown) {
+      player.setVelocityX(160);
+      player.anims.play("anim_tourne_droite", true);
+    }
+
+    // vertical
+    if (clavier.up.isDown) {
+      player.setVelocityY(-160);
+    } else if (clavier.down.isDown) {
+      player.setVelocityY(160);
+    }
+
+    // idling
+    if (player.body.velocity.x === 0 && player.body.velocity.y === 0) {
+      player.anims.play("anim_face", true);
+    }
+
+    if (Phaser.Input.Keyboard.JustDown(clavier.space)) {
+      this.scene.start("niveau1");
+    }
+
+
+    if (clavier.space.isDown) {
+
+      let bullet = bullets.create(player.x, player.y, "img_bullet");
+
+      if (player.body.velocity.x >= 0) {
+        bullet.setVelocityX(400);
+      } else {
+        bullet.setVelocityX(-400);
+      }
+
+    }
   }
-}
 }
