9a5fe703e05c059cdc7d09028aca4e4a51fd8256
Antoine Rouart <antoine.rouart@epfedu.fr>
2026-03-16 11:00:24 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import selection from "./js/selection.js"; 
+import niveau1 from "./js/niveau1.js"; 
+
+var config = {
+  type: Phaser.AUTO,
+  width: 800, // largeur en pixels
+  height: 600, // hauteur en pixels
+  physics: {
+    // définition des parametres physiques
+    default: "arcade", // mode arcade : le plus simple : des rectangles pour gérer les collisions. Pas de pentes
+    arcade: {
+      // parametres du mode arcade
+      gravity: {
+        y: 300 // gravité verticale : acceleration ddes corps en pixels par seconde
+      },
+      debug: true // permet de voir les hitbox et les vecteurs d'acceleration quand mis à true
+    }
+  },
+  scene: [selection,niveau1]  // liste des scenes du jeu : ici une seule scene : selection
+};
+
+
+// création et lancement du jeu à partir de la configuration config
+var game = new Phaser.Game(config);
+game.scene.start("selection"); // lancement de la scene selection
+  
---
+++ src/js/selection.js
@@ -0,0 +1,92 @@
+
+
+var player; // désigne le sprite du joueur
+var groupe_plateformes; // contient toutes les plateformes
+var clavier; // pour la gestion du clavier
+
+export default class selection extends Phaser.Scene {
+  constructor() {
+     super({key : "selection"});}
+
+preload() {
+  // tous les assets du jeu sont placés dans le sous-répertoire src/assets/
+  this.load.image("img_ciel", "src/assets/sky.png");
+  this.load.image("img_plateforme", "src/assets/platform.png");
+  this.load.spritesheet("img_perso", "src/assets/dude.png", {
+    frameWidth: 32,
+    frameHeight: 48
+  });
+}
+
+create() {
+ 
+  this.add.image(400, 300, "img_ciel");
+
+  groupe_plateformes = this.physics.add.staticGroup();
+ 
+  groupe_plateformes.create(200, 584, "img_plateforme");
+  groupe_plateformes.create(600, 584, "img_plateforme");
+
+  //  on ajoute 3 platesformes flottantes
+  groupe_plateformes.create(600, 450, "img_plateforme");
+  groupe_plateformes.create(50, 300, "img_plateforme");
+  groupe_plateformes.create(750, 270, "img_plateforme");
+
+  /****************************
+   *  CREATION DU PERSONNAGE  *
+   ****************************/
+
+  player = this.physics.add.sprite(100, 450, "img_perso");
+  player.setBounce(0.2); // on donne un petit coefficient de rebond
+  player.setCollideWorldBounds(true); // le player se cognera contre les bords du monde
+
+  this.anims.create({
+    key: "anim_tourne_gauche", // key est le nom de l'animation : doit etre unique poru la scene.
+    frames: this.anims.generateFrameNumbers("img_perso", { start: 0, end: 3 }), // on prend toutes les frames de img perso numerotées de 0 à 3
+    frameRate: 10, // vitesse de défilement des frames
+    repeat: -1 // nombre de répétitions de l'animation. -1 = infini
+  });
+
+  // creation de l'animation "anim_tourne_face" qui sera jouée sur le player lorsque ce dernier n'avance pas.
+  this.anims.create({
+    key: "anim_face",
+    frames: [{ key: "img_perso", frame: 4 }],
+    frameRate: 20
+  });
+
+  // creation de l'animation "anim_tourne_droite" qui sera jouée sur le player lorsque ce dernier tourne à droite
+  this.anims.create({
+    key: "anim_tourne_droite",
+    frames: this.anims.generateFrameNumbers("img_perso", { start: 5, end: 8 }),
+    frameRate: 10,
+    repeat: -1
+  });
+
+ 
+  clavier = this.input.keyboard.createCursorKeys();
+
+ 
+
+  //  Collide the player and the groupe_etoiles with the groupe_plateformes
+  this.physics.add.collider(player, groupe_plateformes);
+}
+
+update() {
+  if (clavier.left.isDown) {
+    player.setVelocityX(-160);
+    player.anims.play("anim_tourne_gauche", true);
+  } else if (clavier.right.isDown) {
+    player.setVelocityX(160);
+    player.anims.play("anim_tourne_droite", true);
+  } else {
+    player.setVelocityX(0);
+    player.anims.play("anim_face");
+  }
+
+  if (clavier.up.isDown && player.body.touching.down) {
+    player.setVelocityY(-330);
+  }
+    if (Phaser.Input.Keyboard.JustDown(clavier.space) == true) {
+      this.scene.start("niveau1");
+    } 
+}}
